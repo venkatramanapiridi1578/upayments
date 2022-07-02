57bca5f78d9dfdc1103046a1f37aadd7639fe919
venkatramanapiridi <venkatramanapiridi@gmail.com>
2022-07-02 20:22:36 +0530

    json response format

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -14,6 +14,7 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
+        \App\Http\Middleware\JsonRequestMiddleware::class,
         \App\Http\Middleware\TrustProxies::class,
         \Fruitcake\Cors\HandleCors::class,
         \App\Http\Middleware\CheckForMaintenanceMode::class,
